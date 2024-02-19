fe030859ecbb1a2d05189efbedfb3af197299031
odas0r <guilherme.odas0r@gmail.com>
2024-02-19 11:39:19 +0000

    better test/run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# inject the environment variables
+source "./.env.local"
+
+dir="$(git rev-parse --show-toplevel)"
+
 export GITHUB_SHA="69a3b64e5b36db68b54ca7291d8acfde261dda76"
-# Enter your url.....don't commit this
-export DISCORD_URL=""
+export DISCORD_URL="$DISCORD_CI_CD_WEBHOOK_URL"
 export GITHUB_REPOSITORY="DiscordHooks/github-actions-discord-webhook"
 export GITHUB_REF="refs/heads/4/merge" # refs/heads/feature/new-stuff
 export HOOK_OS_NAME="Windows"
 export WORKFLOW_NAME="Main Pipeline"
 export GITHUB_EVENT_NAME="pull_request" # Could also be push, ext.
-./send.sh Success $DISCORD_URL
+
+"$dir/send.sh" Success $DISCORD_URL
